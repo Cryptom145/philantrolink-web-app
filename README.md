deb12fa1248769c67973af8c72d184687d914282
Cryptom145 <tyronejohn04444@gmail.com>
2024-05-17 19:32:11 +0800

    improved not found page

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Philantrolink Web App (for Admins)
 
 This project utilizes React and Firebase to create the web application of Philantrolink.
+
+## Getting Started
+
+Run npm install on your device after cloning the code to install the necessary node modules.
+
+Run npm run dev to run the application locally.
+
--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -1,7 +1,12 @@
+import React from 'react';
+
 function Error404() {
   return (
-    <div>
-      <h1>Get Started!</h1>
+    <div style={{ backgroundColor: '#073e79', minHeight: '100vh', color: '#ffffff', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <div style={{ textAlign: 'center' }}>
+        <h1 style={{ fontSize: '3em' }}>404 Page Not Found</h1>
+        <p style={{ fontSize: '1.5em' }}>Oops! The page you are looking for could not be found.</p>
+      </div>
     </div>
   );
 }
